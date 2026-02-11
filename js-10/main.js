940352a6a5f6bfda5180e2aa183a97cc0036744e
olfairy-web <ol.raduzhcka@yandex.ru>
2026-02-11 02:09:48 -0800

    Add files via upload

---
+++ js-10/main.js
@@ -0,0 +1,22 @@
+
+let firstName = "Павел";
+let lastName = "Попов";
+let isStudent = true;
+let age = 28;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+console.log(`Студент родился в ${birthYear} году.`);
+
+console.log(`Меня зовут ${firstName}, мне ${age} лет. Я ученик курса ${isStudent}.`);
+
+
+// result= 123456789falsetrue
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+console.log(result);
+
+
